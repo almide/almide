b8c6693454115cd65667ffc1a875a2276fe56c37
O6lvl4 <hiromi.motodera@aid-on.org>
2026-03-30 22:42:50 +0900

    Add install_import_table call to checker unit tests

--- tests/checker_test.rs
+++ tests/checker_test.rs
@@ -9,6 +9,7 @@ fn check(input: &str) -> Vec<(Level, String)> {
     let mut prog = parser.parse().expect("parse failed");
     let diags = {
         let mut checker = Checker::new();
+        checker.install_import_table(&prog);
         checker.check_program(&mut prog)
     };
     diags.into_iter().map(|d| (d.level, d.message)).collect()
@@ -20,6 +21,7 @@ fn check_with_hints(input: &str) -> Vec<(Level, String, String)> {
     let mut prog = parser.parse().expect("parse failed");
     let diags = {
         let mut checker = Checker::new();
+        checker.install_import_table(&prog);
         checker.check_program(&mut prog)
     };
     diags.into_iter().map(|d| (d.level, d.message, d.hint)).collect()
